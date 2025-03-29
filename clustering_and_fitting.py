8d4b095aba0e00e72f2a038e473e5f0066cc8483
Valluru Abhinaya <abhinayavalluru10@gmail.com>
2025-03-29 15:49:51 +0000

    Update clustering_and_fitting.py

--- clustering_and_fitting.py
+++ clustering_and_fitting.py
@@ -136,13 +136,10 @@ def perform_clustering(df, col1, col2):
     plot_elbow_method()
     labels, inertia, cluster_centers = one_silhouette_inertia()
 
-    return (
-        labels, 
-        data1, 
+    return (labels, data1, 
         cluster_centers[:, 0], 
         cluster_centers[:, 1], 
-        cluster_centers
-    )
+        cluster_centers)
 
 
 def plot_clustered_data(labels, data1, xkmeans, ykmeans, centre_labels):
